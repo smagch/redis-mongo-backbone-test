1b426cf7fa2f280cb1a51bba066d909eca82f237
smagch <smagch@gmail.com>
2012-02-22 01:46:23 +0900

    dropbox auth

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,55 @@
 (function($, _, Backbone){
   
+  // main author can block sync.
+  // can be able to see previous saved state
+  // and current state.
+  //console.log('id : ' + id);
   
+  var ArticleModel = Backbone.Model.extend({
+    
+  });
+  
+  var ArticleCollection = Backbone.Collection.extend({
+    model: ArticleModel
+    // url: function() {
+    //   return '/post/' + id;
+    // }
+  });
   
   var AppView = Backbone.View.extend({
     el: '#wrapper',
     initialize: function() {
       this.$input = $('#input');
+      //this.collection = new ArticleCollection();
     },
     events: {
-      'keydown #input': 'sendText'
-    },
-    sendText: function(e) {
-      if(e.keyCode === 13) {
-        var val = this.$input.val();
-        // send text to server
-        
-        
-        this.$input.val('');
-      }
+      //'keydown #input': 'sendText'
     }
+    // sendText: function(e) {
+    //   if(e.keyCode === 13) {
+    //     var val = this.$input.val();
+    //     var model = {
+    //       text: 'this is test model'
+    //     };
+    //     
+    //     $.ajax({
+    //       type: 'POST',
+    //       dataType: 'json',
+    //       contentType: 'application/json',
+    //       url: '/dropbox'
+    //       data: JSON.stringify(model)
+    //     })
+    //     .done(function(res) {
+    //       console.log('done');
+    //       console.log('res : ' + res);
+    //     })
+    //     .fail(function(e) {
+    //       console.log('fail');
+    //       console.dir( e );
+    //     });
+    //     this.$input.val('');
+    //   }
+    //}
   });
   
   var app = new AppView();
--- server.js
+++ server.js
@@ -1,39 +1,64 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , redis = require('redis')
-  , mongoose = require('mongoose')
-  ;
+  , everyauth = require('everyauth')
+  , conf = require('./conf');
 
-var app = module.exports = express.createServer();
+everyauth.debug = true;
 
-// Configuration
+var usersById = {};
+var nextUserId = 0;
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
+function addUser (source, sourceUser) {
+  var user;
+  if (arguments.length === 1) { // password-based
+    user = sourceUser = source;
+    user.id = ++nextUserId;
+    return usersById[nextUserId] = user;
+  } else { // non-password-based
+    user = usersById[++nextUserId] = {id: nextUserId};
+    user[source] = sourceUser;
+  }
+  return user;
+}
+
+var usersByDropboxId = {};
+
+everyauth.everymodule
+  .findUserById( function (id, callback) {
+    callback(null, usersById[id]);
+  });
+
+
+everyauth
+  .dropbox
+    .consumerKey(conf.dropbox.consumerKey)
+    .consumerSecret(conf.dropbox.consumerSecret)
+    .findOrCreateUser( function (sess, accessToken, accessSecret, dropboxUserMetadata) {
+      return usersByDropboxId[dropboxUserMetadata.uid] ||
+        (usersByDropboxId[dropboxUserMetadata.uid] = addUser('dropbox', dropboxUserMetadata));
+    })
+    .redirectPath('/')
+
+var app = express.createServer();
+app.configure( function () {
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'htuayreve' }));
+  app.use(everyauth.middleware());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
-}); 
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.get('/', function (req, res) {
+  res.render('index', {
+    title: 'hoem'
+  });
 });
 
-// Routes
-
-app.get('/', routes.index);
+everyauth.helpExpress(app);
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+console.log('Go to http://local.host:3000');
