18f3a368700ae9962be9bb61ce3b3f51e91b4795
smagch <smagch@gmail.com>
2012-02-25 01:44:19 +0900

    add conf

---
+++ conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+  facebook : {
+    appId: '',
+    appSecret: ''
+  },
+  twitter : {
+    consumerKey: '',
+    consumerSecret: ''
+  },
+  dropbox: {
+    consumerKey: '',
+    consumerSecret: ''
+  }
+}
